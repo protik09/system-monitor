9fadf4f1f57407606b110206c61ff1ce0a72100e
Protik Banerji <protik09@gmail.com>
2020-06-28 14:24:57 +0200

    Install file to compile system-monitor

---
+++ install.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/bash
+
+sudo apt install qt5-default libprocps-dev -y
+
+# Fix an issue with the qt5 core library
+sudo strip --remove-section=.note.ABI-tag /usr/lib/x86_64-linux-gnu/libQt5Core.so.5
