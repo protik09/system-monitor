af9f4e6549080c1b80e0710f0bc700bb1eb7bfc5
Protik Banerji <protik09@gmail.com>
2020-06-28 14:33:36 +0200

    Add make to the install file

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ sudo apt install qt5-default libprocps-dev -y
 
 # Fix an issue with the qt5 core library
 sudo strip --remove-section=.note.ABI-tag /usr/lib/x86_64-linux-gnu/libQt5Core.so.5
+
+# Make the program
+qmake && make -j$(nproc)
